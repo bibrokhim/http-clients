8a313526d081568c0eab9af25b40faa444c08f29
Bakhromjon <bahromjon.ibrohimov@yandex.com>
2023-09-11 10:39:24 +0500

    Refactor SMS dev client

--- src/Clients/SMS/SmsDevClient.php
+++ src/Clients/SMS/SmsDevClient.php
@@ -9,23 +9,18 @@ class SmsDevClient implements SmsClientInterface
 {
     public function send(string $phoneNumber, string $message): void
     {
-        if ($phoneNumber === env('SMS_DEV_NUMBER')) {
-            $smsService = new SmsService();
-            $smsService->send($phoneNumber, $message);
-        } else {
-            Log::info("$phoneNumber: $message");
+        Log::info("$phoneNumber: $message");
 
-            if (! app()->environment('testing')) {
-                Http::get('https://api.telegram.org/bot6135540582:AAHiXJS4tQ5eJDvogGBLps3nfA-OzSI0YFo/sendMessage', [
-                    'chat_id' => 944751850,
-                    'text' => "$phoneNumber: $message"
-                ]);
+        if (! app()->environment('testing')) {
+            Http::get('https://api.telegram.org/bot6135540582:AAHiXJS4tQ5eJDvogGBLps3nfA-OzSI0YFo/sendMessage', [
+                'chat_id' => 944751850,
+                'text' => "$phoneNumber: $message"
+            ]);
 
-                Http::get('https://api.telegram.org/bot6135540582:AAHiXJS4tQ5eJDvogGBLps3nfA-OzSI0YFo/sendMessage', [
-                    'chat_id' => 2707871,
-                    'text' => "$phoneNumber: $message"
-                ]);
-            }
+            Http::get('https://api.telegram.org/bot6135540582:AAHiXJS4tQ5eJDvogGBLps3nfA-OzSI0YFo/sendMessage', [
+                'chat_id' => 2707871,
+                'text' => "$phoneNumber: $message"
+            ]);
         }
     }
 }
