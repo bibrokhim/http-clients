8d0589977ffe89ec611af513cfe5d062c6228cd6
Bakhromjon <bahromjon.ibrohimov@yandex.com>
2023-11-08 09:51:17 +0500

    Add 1c client

--- src/Clients/Helpdesk/HelpdeskClient.php
+++ src/Clients/Helpdesk/HelpdeskClient.php
@@ -2,9 +2,7 @@
 
 namespace Bibrokhim\HttpClients\Clients\Helpdesk;
 
-use App\Models\Device;
 use Bibrokhim\HttpClients\Clients\BaseClient;
-use Illuminate\Http\UploadedFile;
 
 class HelpdeskClient extends BaseClient
 {
---
+++ src/Clients/OneC/OneCClient.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bibrokhim\HttpClients\Clients\OneC;
+
+use Bibrokhim\HttpClients\Clients\BaseClient;
+
+class OneCClient extends BaseClient
+{
+
+}
