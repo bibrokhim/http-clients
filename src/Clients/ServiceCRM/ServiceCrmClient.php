b741c797162cc51b71389a358771a80a130c884a
Bakhromjon <bahromjon.ibrohimov@yandex.com>
2024-12-06 10:21:08 +0500

    Add method to handle device info from mobile app in ServiceCrmClient.

--- src/Clients/ServiceCRM/ServiceCrmClient.php
+++ src/Clients/ServiceCRM/ServiceCrmClient.php
@@ -2,9 +2,19 @@
 
 namespace Bibrokhim\HttpClients\Clients\ServiceCRM;
 
+use App\Models\Device;
 use Bibrokhim\HttpClients\Clients\BaseClient;
 
 class ServiceCrmClient extends BaseClient
 {
-
+    public function fromServiceMobileApp(Device $device): self
+    {
+        return $this->withHeaders([
+            'X-User-ID' => $device->user_id,
+            'X-User-Type' => 'Repairer',
+            'X-User-Platform' => $device->platform,
+            'X-Build' => request()->header('X-Build'),
+            'X-Version' => request()->header('X-Version'),
+        ]);
+    }
 }
