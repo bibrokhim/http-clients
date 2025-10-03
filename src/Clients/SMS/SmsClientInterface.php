3d4d281d139e031deb8c483453e856fa5eb527b5
bekzod <devbekzod@gmail.com>
2025-10-03 14:47:39 +0500

    added whatsapp and telegram sms send

--- src/Clients/SMS/SmsClientInterface.php
+++ src/Clients/SMS/SmsClientInterface.php
@@ -6,5 +6,9 @@ interface SmsClientInterface
 {
     public function send(string $phoneNumber, string $message): void;
 
+    public function sendWhatsapp(string $phoneNumber, string $code): void;
+
+    public function sendTelegram(string $phoneNumber, string $code): void;
+
     public function sendMany(array $phoneNumbers, string $message): void;
 }
--- src/Clients/SMS/SmsGatewayClient.php
+++ src/Clients/SMS/SmsGatewayClient.php
@@ -11,6 +11,16 @@ class SmsGatewayClient extends BaseClient implements SmsClientInterface
         $this->post('/send', compact('phoneNumber', 'message'));
     }
 
+    public function sendWhatsapp(string $phoneNumber, string $code): void
+    {
+        $this->post('/send-whatsapp', compact('phoneNumber', 'code'));
+    }
+
+    public function sendTelegram(string $phoneNumber, string $code): void
+    {
+        $this->post('/send-telegram', compact('phoneNumber', 'code'));
+    }
+
     public function sendMany(array $phoneNumbers, string $message): void
     {
         $this->post('/send-many', compact('phoneNumbers', 'message'));
